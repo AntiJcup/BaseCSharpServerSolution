99c9765f7e33cb593a017a4fec5de7bb61f103f3
AntiJcup <jacobkyle99@yahoo.com>
2020-12-17 09:44:46 -0800

    Add common utilities

---
+++ Utilities/Common/JsonUtils.cs
@@ -0,0 +1,28 @@
+using System.IO;
+using Newtonsoft.Json;
+
+namespace BaseApi.Utilities.Common
+{
+    public class JsonUtils
+    {
+        public static void Serialize(object value, Stream s)
+        {
+            StreamWriter writer = new StreamWriter(s);
+            JsonTextWriter jsonWriter = new JsonTextWriter(writer);
+
+            JsonSerializer ser = new JsonSerializer();
+            ser.Serialize(jsonWriter, value);
+            jsonWriter.Flush();
+        }
+
+        public static T Deserialize<T>(Stream s)
+        {
+            StreamReader reader = new StreamReader(s);
+            using (JsonTextReader jsonReader = new JsonTextReader(reader))
+            {
+                JsonSerializer ser = new JsonSerializer();
+                return ser.Deserialize<T>(jsonReader);
+            }
+        }
+    }
+}
---
+++ Utilities/Common/ProcessExtensions.cs
@@ -0,0 +1,21 @@
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BaseApi.Utilities.Common
+{
+    public static class ProcessExtensions
+    {
+        public static Task WaitForExitAsync(this Process process,
+                CancellationToken cancellationToken = default)
+        {
+            var tcs = new TaskCompletionSource<object>();
+            process.EnableRaisingEvents = true;
+            process.Exited += (sender, args) => tcs.TrySetResult(null);
+            if (cancellationToken != default)
+                cancellationToken.Register(tcs.SetCanceled);
+
+            return tcs.Task;
+        }
+    }
+}
